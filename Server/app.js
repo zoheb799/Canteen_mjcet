bb5fc682515f3a1efb1359a27dfe92a03a72c379
Khaja zohebuddin <96819670+zoheb799@users.noreply.github.com>
2023-07-19 20:28:51 +0530

    Add files via upload

---
+++ Server/app.js
@@ -0,0 +1,16 @@
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+var express = require("express");
+var app = express();
+// var bodyparser = require("Body-Parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+dotenv.config({ path: "./config.env" });
+require("./db/connection");
+app.use(require("./routes/authRoutes"));
+
+const PORT = process.env.PORT;
+app.listen(PORT, function () {
+  console.log("APP RUNNING AT PORT NO 5000");
+});
